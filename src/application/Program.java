64e0d93631d99fe52b8e2d03b340c97f61fd7e82
Bezerra <paulo.victor.bezerra@accenture.com>
2020-09-17 14:25:08 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	Department obj = new Department(1,"books");
-	System.out.println(obj);
+	
+	Seller	seller = new Seller(21,"Bob","bob@email.com", new Date(), 300.0,obj);
+	System.out.println(seller);
 	}
 
 }
